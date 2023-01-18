479379963cd148f27cf3b16857e224f7c7af5a2e
Adiansyah Putra <adiansyahhputra@gmail.com>
2023-01-18 18:16:14 +0700

    alhamdulillah

--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,21 @@ const path = require('path');
 
 const express = require('express');
 
-const routeDir = require('../util/path');
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(routeDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
